6cb20f75e2255e51c097daf3a39dae766cbbd275
ra8-coder <rmvely@gmail.com>
2019-09-17 23:22:20 +0900

    a few revise

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,8 +12,11 @@ const forecast = (longitude, latitude, callback) => {
         }else{
             callback(undefined,
                 body.daily.data[0].summary
-                + ' It is currently ' + body.currently.temperature + ' Celsius. '
+                + ' It is currently ' + body.currently.temperature + ' Celsius.'
                 + 'There is a '+ body.currently.precipProbability + '% chance of rain.'
+                + 'The highest temperature is ' + body.daily.data[0].temperatureHigh 
+                + ' with a low of ' + body.daily.data[0].temperatureLow
+                + '. The coordinates are ' + longitude + ' and ' + latitude
             )
         }
     })
